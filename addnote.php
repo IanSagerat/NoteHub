3e47f651a2bb3815a84651728a185d53a4216178
IanSagerat <125712464+IanSagerat@users.noreply.github.com>
2024-09-18 23:16:17 +0800

    Update addnote.php

--- addnote.php
+++ addnote.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $description = $_POST['description'];
 
         try {
-            $pdo = connectToDatabase();
+            $pdo = connectDB();
             if (!$pdo) {
                 throw new Exception("Failed to connect to the database.");
             }
